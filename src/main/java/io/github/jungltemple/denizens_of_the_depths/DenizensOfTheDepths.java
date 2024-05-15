499933b176a2cdcf00439bb954cff4b78302c0eb
JunglTemple <tubikardu@gmail.com>
2024-05-15 17:31:15 +0100

    Initial Commit :D

---
+++ src/main/java/io/github/jungltemple/denizens_of_the_depths/DenizensOfTheDepths.java
@@ -0,0 +1,10 @@
+package io.github.jungltemple.denizens_of_the_depths;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DenizensOfTheDepths implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Initialization code goes here
+    }
+}
---
+++ src/main/java/io/github/jungltemple/denizens_of_the_depths/client/DenizensOfTheDepthsClient.java
@@ -0,0 +1,10 @@
+package io.github.jungltemple.denizens_of_the_depths.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DenizensOfTheDepthsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization code goes here
+    }
+}
