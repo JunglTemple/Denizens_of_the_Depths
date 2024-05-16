390b0b6df4ffe9fc4f7bd19fede00c26802808b5
JunglTemple <tubikardu@gmail.com>
2024-05-16 16:26:04 +0100

    Begin TESTING phase

--- src/main/java/io/github/jungltemple/denizens_of_the_depths/DenizensOfTheDepths.java
+++ src/main/java/io/github/jungltemple/denizens_of_the_depths/DenizensOfTheDepths.java
@@ -1,10 +1,19 @@
 package io.github.jungltemple.denizens_of_the_depths;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class DenizensOfTheDepths implements ModInitializer {
     @Override
     public void onInitialize() {
         // Initialization code goes here
+        Registry.register(Registries.ITEM, new Identifier("denizens", "test_item"), TEST_ITEM);
+
     }
+
+    public static final Item TEST_ITEM = new Item(new FabricItemSettings());
 }
